78e747ba5225b0ab96ce8983dbb79454d912e48c
Spencer Yoder <z1814808@hopper.cs.niu.edu>
2017-11-30 11:55:17 -0600

    Added an output file

--- shuffle.cc
+++ shuffle.cc
@@ -2,47 +2,62 @@
 #include <algorithm>
 #include <vector>
 #include <cstdlib>
-#include <ctime> 
+#include <ctime>
+#include <fstream>
 
 using namespace std;
 
 int NUM_SIZE=10;
 
-int myrandom (int i) { return std::rand()%i;}
+int myrandom (int i)
+{
+  return std::rand()%i;
+}
 
 int main()
 {
   vector<int> r;
-  int count=0;
+  int count = 0;
+  int filecount = 0;
   srand(time(0));
   
-  for(int i=0; i < NUM_SIZE; i++)
+  ofstream myfile;
+  myfile.open ("output.csv");
+  myfile.open("output" << filecount << ".csv");
+  
+  for(int i=0; i < NUM_SIZE; i++) //Added numbers to the vector
     {
       r.push_back(i);
     }
   
-  random_shuffle(r.begin(), r.end(), myrandom);
+  random_shuffle(r.begin(), r.end(), myrandom); //Do the first shuffle
   
   while (true)
     {
-      if (is_sorted(r.begin(), r.end()))
-	  break;
-
+      if (is_sorted(r.begin(), r.end())) //Check to see if the vector is sorted
+	break;
       
-      random_shuffle(r.begin(), r.end(), myrandom);
-
-      for (int i=0; i < NUM_SIZE; i++)
+      random_shuffle(r.begin(), r.end(), myrandom); //Shuffle the vector
+      
+      for (int i=0; i < NUM_SIZE; i++) //Add the vector contents into the csv
 	{
-	  cout << r[i];
+	  myfile << r[i] << ",";
 	}
-      cout << endl;
       
+      myfile << "\n";
+      
+      if (count >= 995000) //If the file has more than 995000 line in it, Make a new file
+	{
+	  count = 0;
+	  filecount++;
+	  myfile.close();
+	  myfile.open("output" << filecount << ".csv"); //Open the new file
+	}
       count++;
-      cout <<count <<endl;
-      
     }
   
-
+  
+  myfile.close();
   return 0;
-
+  
 }
